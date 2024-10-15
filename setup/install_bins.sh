741bd30fd7afdbb6f5da1e5be13e51a702a04daa
Tristan Kundrat <tristan.kundrat@gmail.com>
2024-10-15 21:48:00 +0200

    floating window tweaks, kitty default window size and fixed install_bins.sh

--- setup/install_bins.sh
+++ setup/install_bins.sh
@@ -3,6 +3,7 @@
 mkdir ~/bin/
 
 wget https://github.com/marcothms/dots/raw/refs/heads/master/files/sway/.config/sway/screenshot --output-document ~/bin/screenshot
+chmod +x ~/bin/screenshot
 
 REPO_DIR=$(git rev-parse --show-toplevel)
-ln -s 
+cp -r $REPO_DIR/bin/* ~/bin/
